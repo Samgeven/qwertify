e31dba169da2d5343bcbdf4e8d02dba8f80e94b0
Чунаев Михаил Михайлович <chunaevmm@askona.ru>
2020-03-16 18:14:04 +0300

    Пофиксил ошибки в консоли при пустой строке

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ function valueToKey(obj) {
 
 function convertKeyLayout(strInput, latin) {
   const letters = {
-  q: "й", w: "ц", e: "у", r: "к", t: "е", y: "н", u: "г", i: "ш", o: "щ", p: "з", "[": "х", "]": "ъ", a: "ф", s: "ы", d: "в", f: "а", g: "п", h: "р", j: "о", k: "л",  l: "д", ";": "ж", "'": "э", z: "я", x: "ч", c: "с", v: "м", b: "и", n: "т", m: "ь", ",": "б", ".": "ю", "{": "Х", "}": "Ъ", ":": "Ж", '"': "Э", ">": "Ю", "<": "Б", "?": ",", "/": "."
+  q: "й", w: "ц", e: "у", r: "к", t: "е", y: "н", u: "г", i: "ш", o: "щ", p: "з", "[": "х", "]": "ъ", a: "ф", s: "ы", d: "в", f: "а", g: "п", h: "р", j: "о", k: "л",  l: "д", ";": "ж", "'": "э", z: "я", x: "ч", c: "с", v: "м", b: "и", n: "т", m: "ь", ",": "б", ".": "ю", "{": "Х", "}": "Ъ", ":": "Ж", '"': "Э", ">": "Ю", "<": "Б", "?": ",", "/": ".", "!": "!", "@": "@", "#": "#", "$": "$", "%": "%", "^": "^", "&": "?", "*" : "*", "(": "(", ")": ")", "`": "ё", "~": "~", "<": "<", ">": ">", "1": "1", "2": "2", "3": "3", "4": "4", "5": "5", "6": "6", "7": "7", "8": "8", "9": "9", "_": "_", "-": "-"
 }
 
 function isUpperCase(str) {
@@ -21,12 +21,13 @@ function isUpperCase(str) {
 }
 
 const lettersReverse = valueToKey(letters);
+const excludedSymbols = /\{|\[|\}|\]|\:|\;|\"|\'|\<|\,|\<|\>|\.|\/|\?|\-|\_|[0-9]/;
 
 strInput = strInput.split(" ");
 for (var j = 0; j < strInput.length; j++) {
   strInput[j] = strInput[j].split("");
   for (var i = 0; i < strInput[j].length; i++) {
-    if (!isUpperCase(strInput[j][i]) || /\{|\[|\}|\]|\:|\;|\"|\'|\<|\,|\<|\>|\.|\/|\?/.test(strInput[j][i])) {
+    if (!isUpperCase(strInput[j][i]) || excludedSymbols.test(strInput[j][i])) {
       if (latin) {
         strInput[j][i] = letters[strInput[j][i]]
       }
@@ -59,8 +60,9 @@ function isLatin(str) {
 
 function getSel() {
     var txtarea;
+    var nodeName = window.getSelection().baseNode.nodeName;
 
-    if (window.getSelection().baseNode.nodeName == "#text") {
+    if (nodeName !== undefined && nodeName == "#text") {
       var txtareaNode = window.getSelection().anchorNode.parentElement
       var selection = window.getSelection().toString();
       var newText = txtareaNode.innerText.replace(selection, convertKeyLayout(selection, isLatin(selection)));
@@ -74,6 +76,10 @@ function getSel() {
       // индексы начала и конца выделенного текста
       var strStart = txtarea.selectionStart;
       var strEnd = txtarea.selectionEnd;
+
+      if (strStart == undefined || strEnd == undefined) {
+        return false
+      }
     
       selection = txtarea.value.substring(strStart, strEnd);
       console.log("выделенный текст - " + selection)
